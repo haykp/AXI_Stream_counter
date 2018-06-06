2f49c95f6d86a3423ca731144ed4c99f7c15281e
haykp <haykpet@gmail.com>
2018-06-06 11:48:17 +0900

    Using same edge of the wclk to drive WE, DIN

--- TB_fifo.sv
+++ TB_fifo.sv
@@ -9,9 +9,14 @@ bit clk, rst, start;
 logic [5:0] cntLimit;
 
 logic [2:0] tdata;
-logic [5 : 0] din = 6'o12;
+logic [5 : 0] din ;
 bit wr_en, rd_en;
 logic full, empty, wr_ack;
+
+logic wr_clk;
+logic rd_clk;
+assign wr_clk = clk;
+assign rd_clk = clk;
 
 // write depth 16
 fifo_generator_0 dataFifo (
@@ -26,27 +31,6 @@ fifo_generator_0 dataFifo (
   .empty(empty)  ,  // output wire empty
   .wr_ack(wr_ack)
 );
-
-initial
-begin
-    wr_en <= 0;
-    @ (negedge full) ;
-    #2 wr_en <= 1;
-    ##16 wr_en <= 0;
-    //##20 wr_en <= 0;
-    //@ (posedge wr_ack)
-    #1 wr_en = 0;
-//    #2 wr_en = 0;
-end
-
-initial 
-begin
-    rd_en <= 0;
-   // @ (negedge full);
-   // ##15 rd_en <= 1;
-   // ##29 rd_en <=0;
-end
-
 initial 
  forever #5 clk++;
 
@@ -55,6 +39,51 @@ begin
     rst <= 1;
     ##4 rst <= 0;
 end
+
+initial
+begin
+    wr_en <= 0;
+	@ (negedge full) ; // wait until FIFO full goes down. 
+	
+	repeat (10 ) 
+	begin
+	@ (posedge wr_clk)
+		wr_en <= 1;
+	@ (posedge wr_clk)	
+		wr_en <= 0;
+	end
+end
+
+initial
+begin
+    din <= 6'd12;
+	@ (negedge full) ; // wait until FIFO full goes down. 
+	
+	repeat (30 ) 
+	begin
+	#5 
+	@ (posedge wr_clk) din <=  din+1;
+	
+	end
+end
+
+initial 
+begin
+    rd_en <= 0;
+ //  @ (posedge full);
+   #300;
+   
+   repeat (10)
+   begin
+   
+	@(posedge rd_clk) rd_en <= rd_en+1;
+   
+   end
+   // ##15 rd_en <= 1;
+   // ##29 rd_en <=0;
+end
+
+
 
 initial
 begin
